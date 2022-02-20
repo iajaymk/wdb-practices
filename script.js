9ff27a6db9514d6b4b31445d003c0aab252940e8
Ajay Kumar <ajaykumarmkaju@gmail.com>
2022-02-21 04:40:33 +0530

    Modified

---
+++ script.js
@@ -0,0 +1,18 @@
+let quantity = document.getElementById("quantity").textContent;
+quantity = Number(quantity);
+quantity = quantity + 5;
+document.getElementById("quantity").textContent = quantity;
+
+function minus(){
+    if(quantity > 1){
+        quantity = quantity - 1;
+        document.getElementById("quantity").textContent = quantity;
+    }
+}
+
+function plus(){
+    if(quantity < 10){
+        quantity = quantity + 1;
+        document.getElementById("quantity").textContent = quantity;
+    }
+}
