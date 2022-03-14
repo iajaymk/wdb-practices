f047c2db2c1e08b1d3a8a69ce5ebfd3c3f81e6a8
Ajay Kumar MK <ajaykumarmkaju@gmail.com>
2022-03-14 17:28:37 +0530

    Added blackjack

---
+++ Blackjack/index.js
@@ -0,0 +1,8 @@
+let firstCard = 12  
+let secondCard = 9
+let message = document.getElementById('message-el') 
+
+function startGame(){
+    message.textContent = "hello"
+    console.log(message)
+}
