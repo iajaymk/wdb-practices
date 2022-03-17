d33c0e0e03c072f71686a86321a90483b4e048e6
Ajay Kumar MK <ajaykumarmkaju@gmail.com>
2022-03-18 03:35:34 +0530

    Finished Black jack

--- Blackjack/index.js
+++ Blackjack/index.js
@@ -1,13 +1,34 @@
-let firstCard = 12  
-let secondCard = 9
-let sum = firstCard + secondCard
-let cards = [firstCard, secondCard]
+
+let sum = 0
+let cards = []
+let isAlive = false
+let hasBlackjack = false
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 let message = document.getElementById("message-el") 
-
+let startEl = document.getElementById("start-el")
 function startGame(){
-    renderGame()
+    isAlive = true
+    let firstCard = randomCard()  
+    let secondCard = randomCard()
+    cards = [firstCard, secondCard]
+    sum = cards[0] + cards[1] 
+    renderGame() 
+    startEl.textContent = "START GAME"
+}
+
+function randomCard(){
+    let card = Math.floor(Math.random()*13) + 1
+    console.log(card)
+    if(card === 1){
+        return card = 11
+    }
+    else if(card > 10){
+        return card = 10
+    }
+    else{
+        return card
+    }
 }
 
 function renderGame(){
@@ -17,19 +38,29 @@ function renderGame(){
         cardsEl.textContent += cards[i] + " "
     }
     if(sum < 21){
+        isAlive = true
         message.textContent = "Draw a new card?"
     }
     else if(sum === 21){
+        isAlive = true
+        hasBlackjack = true
         message.textContent = "You've got Blackjack!"
     }
     else{
+        isAlive = false
         message.textContent = "You are out of the game!"
     }
 }
 
 function newCard(){
-    let card = 7
-    sum += card
-    cards.push(card)
-    renderGame()
+    startEl.textContent = "NEW GAME"
+    if(isAlive === true && hasBlackjack === false){
+        let card = randomCard()
+        sum += card
+        cards.push(card)
+        renderGame()
+    }
+    else{
+        alert(message.textContent)
+    }
 }
