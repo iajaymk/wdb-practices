f1ac2c5721300f1c7959fb4d607b1e4c22ea5684
Ajay Kumar MK <ajaykumarmkaju@gmail.com>
2022-03-17 22:16:06 +0530

    added the new card button

--- Blackjack/index.js
+++ Blackjack/index.js
@@ -1,8 +1,32 @@
 let firstCard = 12  
 let secondCard = 9
-let message = document.getElementById('message-el') 
+let sum = firstCard + secondCard
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+let message = document.getElementById("message-el") 
 
 function startGame(){
-    message.textContent = "hello"
-    console.log(message)
+    renderGame()
+}
+
+function renderGame(){
+    sumEl.textContent = "Sum: " + sum
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+    if(sum < 21){
+        message.textContent = "Draw a new card?"
+    }
+    else if(sum === 21){
+        message.textContent = "You've got Blackjack!"
+    }
+    else{
+        message.textContent = "You are out of the game!"
+    }
+}
+
+function newCard(){
+    let card = 7
+    firstCard = secondCard
+    secondCard = card
+    sum += card
+    renderGame()
 }
