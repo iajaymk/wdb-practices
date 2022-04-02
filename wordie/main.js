f5c605f3a44ae322140a0104b4950f0aa2da1034
Ajay Kumar MK <ajaykumarmkaju@gmail.com>
2022-04-02 23:19:55 +0530

    Added input areas

--- wordie/main.js
+++ wordie/main.js
@@ -3,12 +3,15 @@ const chances = 6
 
 const board = document.getElementById("board")
 
-for(let i = 0; i < chances; i++){
+
+for (let i=0; i< chances; i++){
     const row = document.createElement("div")
     board.appendChild(row)
-
-    for(let j = 0; j < wordLength; j++){
-        const column = document.createElement("input")
-        board.appendChild(column)
+    
+    for(let j=0; j< wordLength; j++){
+        const inputEl = document.createElement("input")
+        inputEl.setAttribute("maxlength",1)
+        board.appendChild(inputEl)
     }
 }
+
